32d8b621425aa15d05b8e640ed5a4516aa100ef7
bonfotieno <bonnyotieno9@gmail.com>
2022-08-17 11:41:27 +0300

    updated

--- src/com/challenge/Soldier.java
+++ src/com/challenge/Soldier.java
@@ -3,17 +3,30 @@ package com.challenge;
 
 public class Soldier {
     private Gun gun;
+    private Tank tank;
     private String militaryId;
     private char type; // A or E
+    private final String shellModel;
     public Soldier(String militaryId) {
         this.militaryId = militaryId;
         this.gun = new Gun();
+        this.tank = new Tank("T127", 100);
+        this.shellModel = tank.getModel();
     }
     public void shootBullets() {
         //System.out.println(this.militaryId + " shooting");
         this.gun.shootBullets();
     }
+    public void shootShells(){
+        this.tank.shootShells();
+    }
     public void changeShootingMode() {
         this.gun.changeShootingMode();
+    }
+    public void changeShellModel(){
+        if (this.shellModel == "canon")
+            this.tank.setModel("long bow");
+        else
+            this.tank.setModel("canon");
     }
 }
