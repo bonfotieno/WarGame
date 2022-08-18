0f65057c1c1794ac6f7c47c7c2f8c182b5cc0368
bonfotieno <bonnyotieno9@gmail.com>
2022-08-18 10:09:24 +0300

    updated

--- src/com/challenge/Soldier.java
+++ src/com/challenge/Soldier.java
@@ -3,6 +3,7 @@ package com.challenge;
 public class Soldier {
     private Gun gun;
     private Tank tank;
+    private Jet jet;
     private String militaryId;
     private char type; // A or E
     private final String shellModel;
@@ -11,22 +12,29 @@ public class Soldier {
         this.type = type;
         this.gun = new Gun();
         this.tank = new Tank("T127", 100);
+        this.jet = new Jet();
         this.shellModel = tank.getModel();
     }
     public void shootBullets() {
-        //System.out.println(this.militaryId + " shooting");
         this.gun.shootBullets();
-    }
-    public void shootShells(){
-        this.tank.shootShells();
     }
     public void changeShootingMode() {
         this.gun.changeShootingMode();
+    }
+    public void shootShells(){
+        this.tank.shootShells();
     }
     public void changeShellModel(){
         if (this.shellModel == "canon")
             this.tank.setModel("long bow");
         else
             this.tank.setModel("canon");
+    }
+    public void jetFiring(){
+        this.jet.fly();
+        this.jet.fire();
+    }
+    public void changeJetType(){
+        this.jet.chooseJetType();
     }
 }
--- src/com/challenge/WarGame.java
+++ src/com/challenge/WarGame.java
@@ -1,27 +1,43 @@
 package com.challenge;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class WarGame {
     private static Soldier[] createSoldiers() {
         Soldier[] soldiers = new Soldier[1000];
         for (int k = 0; k < 1000; k ++) {
-            Soldier soldier = new Soldier("MIL_ID_000747");
+            Soldier soldier = new Soldier("MIL_ID_000747", 'A');
             soldiers[k] = soldier;
         }
         return soldiers;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Army army = new Army();
         army.setSoldiers(createSoldiers());
-        Soldier s = new Soldier("1324ERT");
-        Soldier soldier = new Soldier("MIL_ID_000747");
-        int i = 0;
-        while (i <= 10000) {
-           soldier.shootBullets();
-           if (new Random().nextInt() % 2 == 0)
-               soldier.changeShootingMode();
-           i += 1000;
+        int weapon;
+        System.out.println("*********Start Game***********.\nChoose a weapon:\n1.Gun\n2.Tank\n3.Jet");
+        Scanner scan = new Scanner(System.in);
+        weapon = scan.nextInt();
+        while (true) {
+            Soldier soldier = new Soldier("MIL_ID_000747", 'A');
+            Thread.sleep(2000);
+            int i = 0;
+            while (i <= 10000) {
+                if (weapon == 1) {
+                    soldier.shootBullets();
+                    if (new Random().nextInt() % 2 == 0)
+                        soldier.changeShootingMode();
+                }else if (weapon == 2) {
+                    soldier.shootShells();
+                    if (new Random().nextInt() % 2 == 0)
+                        soldier.changeShellModel();
+                } else if (weapon == 3) {
+                    soldier.jetFiring();
+                    soldier.changeJetType();
+                }
+                i += 1000;
+            }
         }
     }
 }
