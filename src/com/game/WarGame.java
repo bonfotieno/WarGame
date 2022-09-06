b11fc22424d10445ab6cce021fd9f3346a93a98a
bonfotieno <bonnyotieno9@gmail.com>
2022-09-06 13:45:38 +0300

    updated

--- src/com/game/WarGame.java
+++ src/com/game/WarGame.java
@@ -3,8 +3,14 @@ package com.game;
 
 import com.game.controller.WarGameWorld;
 
+import java.io.IOException;
+
 public class WarGame {
-    public static void main(String[] args) throws InterruptedException {
-        new WarGameWorld().run();
+    public static void main(String[] args) {
+        try {
+            new WarGameWorld().run();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
